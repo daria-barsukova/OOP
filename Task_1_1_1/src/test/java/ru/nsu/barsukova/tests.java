9984735ee6517a7ce96ed3bc715afd754831b5eb
daria-barsukova <115018770+daria-barsukova@users.noreply.github.com>
2023-09-10 12:01:21 +0700

    task-1-1-1

---
+++ Task_1_1_1/src/test/java/ru/nsu/barsukova/tests.java
@@ -0,0 +1,34 @@
+package ru.nsu.barsukova;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class tests {
+
+    @Test
+    void test1() {
+        int[] arr = heapsort.sort(new int[]{});
+        assertArrayEquals(arr, new int[]{});
+    }
+
+    @Test
+    void test2() {
+        int[] arr = heapsort.sort(new int[]{5, 4, 3, 2, 1});
+        assertArrayEquals(arr, new int[]{1, 2, 3, 4, 5});
+    }
+
+    @Test
+    void test3() {
+        int[] arr = new int[10000000];
+        for (int i = 0; i < 10000000; i++) {
+            arr[i] = (int) (Math.random() * 10000000);
+        }
+        heapsort.sort(arr);
+        for (int i = 0; i < 9999999; i++) {
+            if (arr[i] > arr[i + 1]) {
+                fail();
+            }
+        }
+    }
+}
