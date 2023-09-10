5e644dc5cc6472bac8ecabb6e3e238b059009898
daria-barsukova <115018770+daria-barsukova@users.noreply.github.com>
2023-09-10 12:29:10 +0700

    Task-1-1-1

---
+++ Task_1_1_1/src/main/java/ru/nsu/barsukova/heapsort.java
@@ -0,0 +1,40 @@
+package ru.nsu.barsukova;
+
+/**
+ * task-1-1-1 heapsort
+ */
+
+public class heapsort {
+
+    static int[] sort(int[] arr) {
+        int n = arr.length;
+        for (int i = n / 2 - 1; i >= 0; i--) {          // heap formation
+            increaseHeap(arr, n, i);
+        }
+        for (int i = n - 1; i >= 0; i--) {              // alternate extraction of elements
+            int tmp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = tmp;
+            increaseHeap(arr, i, 0);
+        }
+        return arr;
+    }
+
+    static void increaseHeap(int[] arr, int n, int i) {
+        int max = i, left, right, tmp;
+        left = 2 * i + 1;
+        right = 2 * i + 2;
+        if (left < n && arr[left] > arr[max]) {          // left child is bigger than root
+            max = left;
+        }
+        if (right < n && arr[right] > arr[max]) {        // right child is bigger than root
+            max = right;
+        }
+        if (max != i) {                                  // maximum element isn`t root
+            tmp = arr[i];
+            arr[i] = arr[max];
+            arr[max] = tmp;
+            increaseHeap(arr, n, max);
+        }
+    }
+}
