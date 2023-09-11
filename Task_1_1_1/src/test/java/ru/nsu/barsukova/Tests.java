75f292d24520b9f929837634f0ba746b9ca0f070
daria-barsukova <115018770+daria-barsukova@users.noreply.github.com>
2023-09-11 11:14:47 +0700

    check style

--- Task_1_1_1/src/test/java/ru/nsu/barsukova/Tests.java
+++ Task_1_1_1/src/test/java/ru/nsu/barsukova/Tests.java
@@ -1,7 +1,6 @@
 package ru.nsu.barsukova;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
