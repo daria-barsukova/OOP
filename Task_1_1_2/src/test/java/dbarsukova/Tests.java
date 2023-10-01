792fbcd0d08b70b1173b14541048e64d05944918
d.barsukova <d.barsukova@g.nsu.ru>
2023-10-01 14:47:24 +0700

    some cringe stuff fixed

--- Task_1_1_2/src/test/java/dbarsukova/Tests.java
+++ Task_1_1_2/src/test/java/dbarsukova/Tests.java
@@ -50,7 +50,7 @@ public class Tests {
     @Test
     void test6() {
         Polynomial p1 = new Polynomial(new int[]{6, 0, -3, 5, -6});
-        assertEquals(p1.differentiate(-1).toString(), "144x + 30");
+        assertEquals(p1.differentiate(3).toString(), "144x + 30");
     }
 
     @Test
