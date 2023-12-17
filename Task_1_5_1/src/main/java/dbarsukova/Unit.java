7f341b41c2847ba07bab911f009c108c73ac36c2
d.barsukova <d.barsukova@g.nsu.ru>
2023-12-17 14:54:10 +0700

    try..

--- Task_1_5_1/src/main/java/dbarsukova/Unit.java
+++ Task_1_5_1/src/main/java/dbarsukova/Unit.java
@@ -1,4 +1,43 @@
 package dbarsukova;
 
-public record Unit(Expression exp, Expression.Type type) {
+import java.util.Objects;
+
+public final class Unit {
+    private final Expression exp;
+    private final Expression.Type type;
+
+    public Unit(Expression exp, Expression.Type type) {
+        this.exp = exp;
+        this.type = type;
+    }
+
+    public Expression exp() {
+        return exp;
+    }
+
+    public Expression.Type type() {
+        return type;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Unit) obj;
+        return Objects.equals(this.exp, that.exp) &&
+                Objects.equals(this.type, that.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(exp, type);
+    }
+
+    @Override
+    public String toString() {
+        return "Unit[" +
+                "exp=" + exp + ", " +
+                "type=" + type + ']';
+    }
+
 }
