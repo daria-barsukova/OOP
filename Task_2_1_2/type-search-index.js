d72631b642b92494ca20c5733ab992197be56f71
daria-barsukova <daria-barsukova@users.noreply.github.com>
2024-03-16 11:50:54 +0000

    Deploying to gh-pages from @ daria-barsukova/OOP@47149e7094866b56af184126ab8847e9c4d4d101 🚀

---
+++ Task_2_1_2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"dbarsukova","l":"Client"},{"p":"dbarsukova","l":"HandlerForClient"},{"p":"dbarsukova","l":"HandlerForConnections"},{"p":"dbarsukova","l":"IsPrime"},{"p":"dbarsukova","l":"Server"}]
