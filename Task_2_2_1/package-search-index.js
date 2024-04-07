4074606799156559e27b8deeeb9fdbcd0280e2f4
daria-barsukova <daria-barsukova@users.noreply.github.com>
2024-04-07 17:04:51 +0000

    Deploying to gh-pages from @ daria-barsukova/OOP@ded1515f97a634af9ac2148726cb11978a5ba829 🚀

---
+++ Task_2_2_1/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"dbarsukova.application"},{"l":"dbarsukova.baker"},{"l":"dbarsukova.customer"},{"l":"dbarsukova.order"},{"l":"dbarsukova.pizzeria"},{"l":"dbarsukova.queue"}]
