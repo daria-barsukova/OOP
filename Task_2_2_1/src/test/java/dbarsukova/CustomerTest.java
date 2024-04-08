ffed125f2fe9fc4599a320205282ee31d52957f9
d.barsukova <d.barsukova@g.nsu.ru>
2024-04-08 13:09:27 +0700

    checkstyle

--- Task_2_2_1/src/test/java/dbarsukova/CustomerTest.java
+++ Task_2_2_1/src/test/java/dbarsukova/CustomerTest.java
@@ -3,10 +3,8 @@ package dbarsukova;
 import dbarsukova.baker.Bakers;
 import dbarsukova.order.Order;
 import dbarsukova.queue.MyQueue;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
@@ -15,26 +13,25 @@ import org.junit.jupiter.api.Test;
  */
 
 class CustomerTest {
+    private static Bakers bakers;
+    private static MyQueue<Order> orderQueue;
+
     @BeforeAll
     static void setUp() {
-    orderQueue = new MyQueue<>(0);
-    bakers = new Bakers(orderQueue);
+        orderQueue = new MyQueue<>(0);
+        bakers = new Bakers(orderQueue);
     }
 
     @Test
-    @DisplayName("Customers")
-    void customers_test() {
-    Assertions.assertTrue(orderQueue.isEmpty());
-    Thread customersThread = new Thread(bakers);
-    customersThread.start();
+    void test1() {
+        Assertions.assertTrue(orderQueue.isEmpty());
+        Thread customersThread = new Thread(bakers);
+        customersThread.start();
         try {
-            Thread.sleep(SLEEP_TIME);
-        } catch (InterruptedException ignored) {}
+            Thread.sleep(1000);
+        } catch (InterruptedException ignored) {
+        }
         bakers.stop();
-    Assertions.assertFalse(orderQueue.isEmpty());
+        Assertions.assertFalse(orderQueue.isEmpty());
     }
-
-    private static Bakers bakers;  // for generating orders
-    private static MyQueue<Order> orderQueue;  // list of orders
-    private final static int SLEEP_TIME = 1000;  // time for testing
 }
